e696919da35dc247997de9790fdbc60c454ea2dc
SeymaDemir <seymademir35@gmail.com>
2022-12-01 10:40:04 +0300

    added useState for category selection

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import {CATEGORIES as categories} from "../data"
 
 function CategoryFilter() {
+  const [selected, setSelected] = useState(null)
+
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {categories.map((category, index) => {
-        return <button key={index} type="button">{category}</button>
+        return <button key={index} type="button" 
+        className={selected !== index ? "" : "selected"} 
+        onClick={() => setSelected(index)} index={index}>
+          {category}
+        </button>
       })}
     </div>
   );
